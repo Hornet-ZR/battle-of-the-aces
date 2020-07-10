fa54f50e3de281f2d77b40377797cf927e27fbd5
awewsomegamer <>
2020-07-09 17:30:54 -0700

    Created some methods

--- battle of the aces/src/main/main.java
+++ battle of the aces/src/main/main.java
@@ -1,6 +1,21 @@
 package main;
 
 public class main {
+	private Thread mainLoop;
+	private boolean running = false;
+	private double frame_cap = 1.0/120.0;
+	
+	public void init() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void loop() {
+		
+	}
 	public static void main (String args[]){
 		System.out.println("Hello WOrld");
 	}
